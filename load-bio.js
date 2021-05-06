e6c7133f0dc039216cd1b32397233a6d44a5f90d
Duong Nguyen <duongnguyen@Duongs-MacBook-Pro.local>
2021-05-05 19:14:11 -0700

    fixed spacing problem

--- load-bio.js
+++ load-bio.js
@@ -1,12 +1,13 @@
+let bioTxt = `
 Quang Nguyen
 Software Engineer | Full Stack Developer | Front End Developer | Backend Developer | Mobile Application Developer
-        
+
 ---
 
 Hi! I'm Quang, self-taught/bootcamp graduate full-stack developer based in Orange County, California.
 3 years of experience in building mobile applications for Android & iOS.
 2 years of experience in full-stack development, developing frontend web applications and backend services based on the microservices architecture.
-        
+
 ---
 
 Languages: Typescript/Javascript/ES6, Unix/Bash, Bat/Windows CMD, Python, Swift.
@@ -23,5 +24,8 @@ Bored? Play this: <a href="https://quangnguyen17.github.io/find-waldo.html">http
 
 ---
 
-<a href="https://github.com/quangnguyen17">GitHub</a> | <a href="https://www.instagram.com/quangd_nguyen/">Instagram</a>
-Email me at <a href="mailto:wan15112001@gmail.com">wan15112001@gmail.com</a>
+<a href="https://github.com/quangnguyen17">GitHub</a> | <a href="http://https://www.linkedin.com/in/quangnguyen2001/">LinkedIn</a> | <a href="https://www.instagram.com/quangd_nguyen/">Instagram</a>
+Email me at <a href="mailto:wan15112001@gmail.com">wan15112001@gmail.com</a> 
+`;
+
+document.querySelector('#bio').innerHTML = bioTxt;
